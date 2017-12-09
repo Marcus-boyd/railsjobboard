72c477ece825bac733d915b3b322dd1696927b68
Marcus-boyd <marcus.boyd389@gmail.com>
2017-12-09 01:29:54 -0500

    add devise gem and toastr gem

--- Gemfile
+++ Gemfile
@@ -17,6 +17,9 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
+gem 'devise'
+gem 'toastr-rails'
+
 
 group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :last_name, :first_name])
+  # end
+
+  def after_sign_in_path_for(resource)
+    jobs_path
+  end
+
 end
